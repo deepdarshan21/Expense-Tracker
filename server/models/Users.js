b09f695d720dd6ea8b8df3b9a39b83b71b7ce0b9
Deepdarshan <deepdarshan21@gmail.com>
2022-01-24 01:13:04 +0530

    Model Update

--- server/models/Users.js
+++ server/models/Users.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     date: { type: Date, default: Date.now },
+    budget: {type: Number, default: 0},
     expense: [
         {
             name: { type: String },
